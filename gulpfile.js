85e1ff8da26ccdf46997d1a651c89804d1a45473
geran1ka <roman.khoruzhy@gmail.com>
2023-08-01 20:51:47 +0300

    Добавлен обработка препроцессор sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,90 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
-import cssImport from 'gulp-cssimport';
+import sassPkg from 'sass';
+import gulpSass from 'gulp-sass';
 import gulpCssimport from 'gulp-cssimport';
-import {deleteAsync} from 'del';//задачи
+import {deleteAsync} from 'del';
+
+const prepros = false;
+
+const sass = gulpSass(sassPkg);
+
+// задачи
 
 export const html = () => gulp
-  .src('src/*.html')
-  .pipe(gulp.dest('dist'))
-  .pipe(browserSync.stream());
+    .src('src/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
 
-  export const css = () => gulp
-  .src('src/css/index.css') // когда файлов много то можно использовать **/*.css правильный путь (src/css/index.css)
-  .pipe(cssImport({
-    extensios: ['css'],
-  }))
-  .pipe(gulp.dest('dist/css'))
-  .pipe(browserSync.stream());
+export const css = () => gulp
+    .src('src/css/index.css') // когда файлов много то можно использовать **/*.css правильный путь (src/css/index.css)
+    .pipe(gulpCssimport({
+      extensios: ['css'],
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
 
-  export const js = () => gulp
-  .src('src/js/**/*.js')
-  .pipe(gulp.dest('dist/js'))
-  .pipe(browserSync.stream());
+export const style = () => {
+  if (prepros) {
+    return gulp
+        .src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+  }
 
-  export const copy = () => gulp
+  return gulp
+      .src('src/css/index.css') // когда файлов много то можно использовать **/*.css правильный путь (src/css/index.css)
+      .pipe(gulpCssimport({
+        extensios: ['css'],
+      }))
+      .pipe(gulp.dest('dist/css'))
+      .pipe(browserSync.stream());
+};
+
+export const js = () => gulp
+    .src('src/js/**/*.js')
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
+
+export const copy = () => gulp
     .src([
       'src/fonts/**/*',
       'src/img/**/*', // 'src/assets/**/*.{png, jpg, jpeg, svg}
     ], {
-      base: 'src'
+      base: 'src',
     })
     .pipe(gulp.dest('dist'))
     .pipe(browserSync.stream({
       once: true,
-    }))
-  
+    }));
 
-  export const server = () => {
-    browserSync.init({
-      ui: false,
-      notify: false,
-      // tunnel: true, // используется когда необходимо показать сайт заказчику
-      server: {
-        baseDir: 'dist'
-      }
-    })
 
-    gulp.watch('./src/**/*.html', html);
-    gulp.watch('./src/css/**/*.css', css);
-    gulp.watch('./src/js/**/*.js', js);
-    gulp.watch([
-      './src/img/**/*',
-      './src/fonts/**/*'
-    ], copy);
-  };
+export const server = () => {
+  browserSync.init({
+    ui: false,
+    notify: false,
+    // tunnel: true, // используется когда необходимо показать сайт заказчику
+    server: {
+      baseDir: 'dist',
+    },
+  });
 
-  export const clear = async () => await deleteAsync('dist/**/*', {forse: true,});
+  gulp.watch('./src/**/*.html', html);
+  gulp.watch(prepros ? './src/scss/**/*.scss' : './src/css/**/*.css', style);
+  gulp.watch('./src/js/**/*.js', js);
+  gulp.watch([
+    './src/img/**/*',
+    './src/fonts/**/*',
+  ], copy);
+};
 
-  // запуск
-  export const base = gulp.parallel(html, css, js, copy);
+export const clear = async () => await deleteAsync('dist/**/*', {forse: true});
 
-  export const build = gulp.series(clear, base);
+// запуск
+export const base = gulp.parallel(html, style, js, copy);
 
-  export default gulp.series(base, server);
-  
+export const build = gulp.series(clear, base);
+
+export default gulp.series(base, server);
+
